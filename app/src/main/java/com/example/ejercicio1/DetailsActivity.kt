95305eabb64712043268bcccc36ac1bcad6355cc
Hector <hectoreduardo354@hotmail.com>
2022-11-13 22:04:34 -0600

    Aun sin implementar material DatePicker pero quedo resuelto el issue de que no se mostraba el texto en los text views, era por el databinding especificamente por el binding.root

--- app/src/main/java/com/example/ejercicio1/DetailsActivity.kt
+++ app/src/main/java/com/example/ejercicio1/DetailsActivity.kt
@@ -1,15 +1,53 @@
 package com.example.ejercicio1
 
+import android.app.appsearch.StorageInfo
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.ejercicio1.databinding.ActivityDetailsBinding
 
+@Suppress("DEPRECATION")
 class DetailsActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityDetailsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_details)
+        binding = ActivityDetailsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val bundle = intent.extras
+
+        var edad:String
+        val nombre = bundle?.getString("nombre","byDefault")
+        val fechaNacimiento = bundle?.getString("fecha","byDefault")
+        val numeroCuenta = bundle?.getString("numeroCuenta","byDefault")
+        val email = bundle?.getString("correo","byDefault")
+
+        calcularEdad(fechaNacimiento)
+        obtenerSignoChino()
+        obtenerSignoZodiacal()
+
+        with(binding){
+            tvNombre.text = nombre
+            tvEdad.text = "73"
+            tvFechaNacimiento.text = fechaNacimiento
+            tvNumeroCuenta.text = numeroCuenta
+            tvCorreoElectronico.text = email
+        }
     }
 
+    private fun obtenerSignoZodiacal() {
+
+    }
+
+    private fun obtenerSignoChino() {
+
+    }
+
+    fun calcularEdad(fecha:String?){
+
+    }
+
+    @Deprecated("Deprecated in Java")
     override fun onBackPressed() {
         super.onBackPressed()
         startActivity(Intent(this, MainActivity::class.java))
--- app/src/main/java/com/example/ejercicio1/MainActivity.kt
+++ app/src/main/java/com/example/ejercicio1/MainActivity.kt
@@ -5,18 +5,78 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Toast
+import com.example.ejercicio1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 
     fun click(view: View) {
-        //Manejamos el click del elemento en el recycler view
-        val intent = Intent(this,DetailsActivity::class.java )
-        //intent.putExtra("ID",movie.id)
-        startActivity(intent)
-        finish()
+        var parametros = Bundle()
+
+        with(binding) {
+            when {
+                tietNombre.text.toString().isEmpty() -> {
+                    tietNombre.error = "No puede quedar vacío"
+                    Toast.makeText(
+                        this@MainActivity,
+                        "Por favor llene todos los campos",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                tietNacimiento.text.toString().isEmpty() -> {
+                    tietNacimiento.error = "No puede quedar vacío"
+                    Toast.makeText(
+                        this@MainActivity,
+                        "Por favor llene todos los campos",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                tietNumCuenta.text.toString().isEmpty() -> {
+                    tietNumCuenta.error = "No puede quedar vacío"
+                    Toast.makeText(
+                        this@MainActivity,
+                        "Por favor llene todos los campos",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                tietCorreo.text.toString().isEmpty() -> {
+                    tietCorreo.error = "No puede quedar vacío"
+                    Toast.makeText(
+                        this@MainActivity,
+                        "Por favor llene todos los campos",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }else -> {
+                    if(true){
+                        //Toast.makeText(this@MainActivity,"Gracias",Toast.LENGTH_SHORT).show()
+                        parametros.apply{
+                            putString("nombre", tietNombre.text.toString())
+                            putString("fecha",tietNacimiento.text.toString())
+                            putString("numeroCuenta",tietNumCuenta.text.toString())
+                            putString("correo",tietCorreo.text.toString())
+                        }
+
+                        val intent = Intent(this@MainActivity, DetailsActivity::class.java)
+                        intent.putExtras(parametros)
+                        startActivity(intent)
+                        finish()
+                    } else {
+                        Toast.makeText(
+                            this@MainActivity,
+                            "Error al actualizar el registro",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+
+                }
+            }
+        }
     }
 }
